690b5073bba6e12c7f677d3c71b73225198e703c
enrique-cabrini <enrique.cabrini@gmail.com>
2025-01-03 10:58:14 +0000

    added the cards and a variable to store the card the player has been dealt

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
+    let cards = 
+        (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14);
+
     println!("Welcome to Blackjack");
+
+    let player_card = cards.13;
+    println!("{player_card}")
 }
