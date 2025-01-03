e4f7c81412d411d9fdec19562dbed9af6a819e79
enrique-cabrini <enrique.cabrini@gmail.com>
2025-01-03 12:06:27 +0000

    added functions to deal cards to player and dealer

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
-fn main() {
-    let cards = 
+const CARDS: (u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8) = 
         (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14);
 
+fn main() {
     println!("Welcome to Blackjack");
 
-    let player_card = cards.13;
-    println!("{player_card}")
+    let player_card = deal_player_card();
+    println!("{player_card}");
+
+    let dealer_card = deal_dealer_card();
+    println!("{dealer_card}");
+}
+
+fn deal_player_card() -> u8 {
+    CARDS.13
+}
+
+fn deal_dealer_card() -> u8 {
+    CARDS.13
 }
