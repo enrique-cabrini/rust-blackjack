5cc2e3f5b086bd5579141bd1c36c82c228e36274
enrique-cabrini <enrique.cabrini@gmail.com>
2025-01-11 15:07:52 +0000

    added player buy in and bet placing

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,43 @@
+struct Player {
+    amount: u8
+}
+
+impl Player {
+    fn buy_in(buy_in_amount: u8) -> Player {
+        Self {
+            amount: buy_in_amount
+        }
+    }
+
+    fn place_bet(&mut self, bet_amount: u8) {
+        self.amount -= bet_amount;
+    }
+}
+
 const CARDS: (u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8) = 
         (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14);
 
 fn main() {
-    println!("Welcome to Blackjack");
+    println!("Welcome to Blackjack\n
+        ************************************");
+    
+    let mut player = Player::buy_in(10);
+    println!("\nYou bought in for $10.00");
 
-    let player_card = deal_player_card();
-    println!("{player_card}");
+    while player.amount != 0 {
+        player.place_bet(1);
+        println!("\nYou have ${}.00 left", player.amount);
 
-    let dealer_card = deal_dealer_card();
-    println!("{dealer_card}");
+        let player_cards = deal_first_cards();
+        println!("Player Cards:\n{} {}", 
+            player_cards[0], player_cards[1]);
+
+        let dealer_cards = deal_first_cards();
+        println!("Dealer Cards:\n{} {}",
+            dealer_cards[0], dealer_cards[1]);
+    }
 }
 
-fn deal_player_card() -> u8 {
-    CARDS.13
-}
-
-fn deal_dealer_card() -> u8 {
-    CARDS.13
+fn deal_first_cards() -> Vec<u8> {
+    vec![CARDS.1, CARDS.13]
 }
